0c8b5b1bb2dc557adf48e944f38f7e08c519b0e1
Jacqueline Lo <lojacqueline2@gmail.com>
2021-04-20 15:25:40 -0400

    steps added

--- project1.py
+++ project1.py
@@ -7,7 +7,7 @@ def display_board(board):
     print( board[1] + '|' +board[2]+ '|' + board[3])
 
 
-# board = ['#',' ',' ',' ',' ',' ' ,' ',' ',' ',' ' ]
+board = ['#',' ',' ',' ',' ',' ' ,' ',' ',' ',' ' ]
 # test_board = ['#','X','O','X','O','X','O','X','O','X']
 # display_board(board)
 # display_board(test_board)
@@ -24,5 +24,34 @@ def player_input():
         player2 = "O"
     else: 
         player2= "X"
+    return player1, player2
 
-player_input()
+# to unpack tuples for use 
+# player1marker, player2marker = player_input()
+
+# print ('Player 1 Marker:' +player1marker + ' ' +'Player 2 Marker:' +player2marker)
+
+# Step 3: Write a function that takes in the board list object, a marker ('X' or 'O'), and a desired position (number 1-9) and assigns it to the board.
+def place_marker(board, marker, position):
+    if board[position] == " ":
+       board[position] = marker
+    else: 
+        print ('Please choose different position')
+
+place_marker(board,'$',8)
+display_board(board)
+
+# **Step 4: Write a function that takes in a board and a mark (X or O) and then checks to see if that mark has won. **
+def win_check(board, mark):
+
+# Step 5: Write a function that uses the random module to randomly decide which player goes first. You may want to lookup random.randint() Return a string of which player went first.
+
+# **Step 6: Write a function that returns a boolean indicating whether a space on the board is freely available.**
+
+# **Step 7: Write a function that checks if the board is full and returns a boolean value. True if full, False otherwise.**
+
+# Step 8: Write a function that asks for a player's next position (as a number 1-9) and then uses the function from step 6 to check if it's a free position. If it is, then return the position for later use.
+
+# **Step 9: Write a function that asks the player if they want to play again and returns a boolean True if they do want to play again.**
+
+# **Step 10: Here comes the hard part! Use while loops and the functions you've made to run the game!**
